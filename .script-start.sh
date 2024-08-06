11e68910dc9d9e91acc3f9a80b2a588dde9d1a62
iscomandev <iscomandev@outlook.com>
2024-08-06 14:50:53 +0800

    chore: script update

--- .script-start.sh
+++ .script-start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # nvm use
-nohup pnpm run start > ./.run.log 2>&1 & echo $! > ./.run.pid
+#nohup pnpm run start > ./.run.log 2>&1 & echo $! > ./.run.pid
+# TODO temp 临时这么干
+nohup pnpm run dev > ./.run.log 2>&1 & echo $! > ./.run.pid
